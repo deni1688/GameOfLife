402cf663b43feb10754f1c7d2f8a5ee93d8dcc37
denis <denis.junuzovic@fleetster.net>
2020-10-16 09:01:29 +0200

    grid is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+
+const CELL_SIZE = 15
+
+type CellProps = { c: number, setLife: (n: number) => void };
+
+function Cell({c, setLife}: CellProps) {
+    return <div style={{
+        width: CELL_SIZE + 'px',
+        height: CELL_SIZE + 'px',
+        backgroundColor: c ? "green" : "",
+        border: "1pt solid green",
+    }} onClick={() => c ? setLife(0) : setLife(1)}/>;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [grid, setGrid] = useState([] as Array<number[]>);
+    const [rows] = useState(40);
+    const [cols] = useState(80);
+    const [running, setRunning] = useState(false);
+
+    useEffect(() => {
+        setGrid(Array(rows).fill(0).map(() => Array(cols).fill(0)));
+    }, [rows, cols, setGrid]);
+
+
+
+    return (
+        <div>
+            <div>
+                <button style={{margin: '10px 10px'}} onClick={() => setRunning(!running)}>
+                    {running ? 'Stop Simulation' : 'Start Simulation'}
+                </button>
+            </div>
+            <div style={{
+                display: 'grid',
+                gridTemplateColumns: `repeat(${cols}, ${CELL_SIZE}px)`,
+                gap: '1px 4px'
+            }}>
+                {grid.map((r, ri) => r.map((c, ci) => <Cell
+                    key={`${ri}-${ci}`}
+                    c={c}
+                    setLife={(v: number) => setGrid(grid => {
+                        const gridCopy = grid.slice();
+
+                        gridCopy[ri][ci] = v;
+
+                        return gridCopy;
+                    })}
+                />))}
+            </div>
+        </div>
+    );
 }
 
 export default App;
